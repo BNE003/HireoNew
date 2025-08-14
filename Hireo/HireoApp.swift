cbb09fd72bd5b85d05c1a72a2b3029f0f101c275
BNE003 <94901423+BNE003@users.noreply.github.com>
2025-08-14 23:09:43 +0200

    Initial Commit

---
+++ Hireo/HireoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HireoApp.swift
+//  Hireo
+//
+//  Created by Benedikt Held on 14.08.25.
+//
+
+import SwiftUI
+
+@main
+struct HireoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HireoTests/HireoTests.swift
@@ -0,0 +1,17 @@
+//
+//  HireoTests.swift
+//  HireoTests
+//
+//  Created by Benedikt Held on 14.08.25.
+//
+
+import Testing
+@testable import Hireo
+
+struct HireoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
